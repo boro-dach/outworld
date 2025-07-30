7864128a750b58b475fb53aa27b68c5c83938a11
boro-dach <140444699+boro-dach@users.noreply.github.com>
2025-07-31 01:46:19 +0200

    fix admin article entity

--- widgets/articles/ui/list-admin.tsx
+++ widgets/articles/ui/list-admin.tsx
@@ -1,20 +1,7 @@
 "use client";
-<<<<<<< Updated upstream
-<<<<<<< HEAD:widgets/news/ui/list.tsx
-<<<<<<< Updated upstream:widgets/news/ui/list.tsx
-import { useNewsQuery } from "@/entities/new/model/use-news";
-import New from "@/entities/new/ui/new";
-=======
-=======
->>>>>>> Stashed changes
 import { ArticleCategory } from "@/entities/article/model/enums";
 import { useArticlesQuery } from "@/entities/article/model/use-articles";
 import JournalistArticle from "@/entities/article/ui/article-admin";
->>>>>>> Stashed changes:widgets/articles/ui/list-admin.tsx
-=======
-import { useArticlesQuery } from "@/entities/article/model/use-articles";
-import JournalistArticle from "@/entities/article/ui/article-admin";
->>>>>>> 4fc796df0ae0c2c70136086e8d73721bd7cfbf9e:widgets/articles/ui/list-admin.tsx
 import { formatDate } from "@/shared/lib/date/date-formatter";
 import React from "react";
 
@@ -23,20 +10,9 @@ type ArticleType = {
   text: string;
   title: string;
   createdAt: string;
-<<<<<<< HEAD:widgets/news/ui/list.tsx
-<<<<<<< Updated upstream:widgets/news/ui/list.tsx
-=======
   likes: number;
   isLiked: boolean;
   type: ArticleCategory;
-<<<<<<< Updated upstream
->>>>>>> Stashed changes:widgets/articles/ui/list-admin.tsx
-=======
-  likes: number;
-  isLiked: boolean;
->>>>>>> 4fc796df0ae0c2c70136086e8d73721bd7cfbf9e:widgets/articles/ui/list-admin.tsx
-=======
->>>>>>> Stashed changes
 };
 
 const JournalistArticlesList = () => {
@@ -53,20 +29,10 @@ const JournalistArticlesList = () => {
           text={article.text}
           title={article.title}
           date={formatDate(article.createdAt)}
-<<<<<<< HEAD:widgets/news/ui/list.tsx
-<<<<<<< Updated upstream:widgets/news/ui/list.tsx
-=======
           likes={article.likes}
           isLiked={article.isLiked}
           type={article.type}
           id={article.id}
->>>>>>> Stashed changes:widgets/articles/ui/list-admin.tsx
-=======
-          likes={article.likes}
-          isLiked={article.isLiked}
-          type={article.type}
-          id={article.id}
->>>>>>> 4fc796df0ae0c2c70136086e8d73721bd7cfbf9e:widgets/articles/ui/list-admin.tsx
         />
       ))}
       {data?.length === 0 && (
