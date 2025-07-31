379c86ddd35f7a1c7040961dcdc72ee97b07d636
boro-dach <140444699+boro-dach@users.noreply.github.com>
2025-07-31 02:10:34 +0200

    Update like.ts

--- features/article/like/api/like.ts
+++ features/article/like/api/like.ts
@@ -12,7 +12,7 @@ export async function Like(values: any) {
     }
 
     const response = await axios.post(
-      "http://localhost:5000/article/like",
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/article/like`,
       values,
       {
         headers: {
